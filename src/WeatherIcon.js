fed26b4b58fbaab0c209439739e51c88bfa85e73
codingpaige <paige_ng@outlook.com>
2023-12-15 13:21:05 +0800

    Mapped SVG weather icons to Openweather API's default icons.

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,34 +1,32 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   console.log(props.code);
   const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "PARTLY_CLOUDY_DAY",
-    "03n": "PARTLY_CLOUDY_NIGHT",
-    "04d": "PARTLY_CLOUDY_DAY",
-    "04n": "PARTLY_CLOUDY_NIGHT",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10d": "RAIN",
-    "10n": "RAIN",
-    "11d": "RAIN",
-    "11n": "RAIN",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG",
+    "01d": "clear-day.svg",
+    "01n": "clear-night.svg",
+    "02d": "partly-cloudy.svg",
+    "02n": "partly-cloudy-night.svg",
+    "03d": "cloudy.svg",
+    "03n": "cloudy.svg",
+    "04d": "overcast-day.svg",
+    "04n": "overcast-night.svg",
+    "09d": "drizzle.svg",
+    "09n": "drizzle.svg",
+    "10d": "partly-cloudy-day-rain.svg",
+    "10n": "partly-cloudy-night-rain.svg",
+    "11d": "thunderstorms.svg",
+    "11n": "thunderstorms.svg",
+    "13d": "snow.svg",
+    "13n": "snow.svg",
+    "50d": "mist.svg",
+    "50n": "mist.svg",
   };
   return (
-    <ReactAnimatedWeather
-      icon={codeMapping[props.code]}
-      color="coral"
-      size={64}
-      animate={true}
+    <img
+      src={`/animatedweathericons/${codeMapping[props.code]}`}
+      alt="weather icon"
+      width="100px"
     />
   );
 }
