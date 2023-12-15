ffa91d4b4be30ddb37daa88358bf0135aa481b5f
codingpaige <paige_ng@outlook.com>
2023-12-15 14:46:56 +0800

    Added temperature conversion feature.

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="temperature">
-      <span className="temp-value">{Math.round(props.celsius)}</span>
-      <span className="temp-metrics">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="temperature">
+        <span className="temp-value">{Math.round(props.celsius)}</span>
+        <span className="temp-metrics">°C | </span>
+        <span className="temp-metrics">
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="temperature">
+        <span className="temp-value">{Math.round(fahrenheit())}</span>
+        <span className="temp-metrics">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          |{" "}
+        </span>
+        <span className="temp-metrics">°F</span>
+      </div>
+    );
+  }
 }
